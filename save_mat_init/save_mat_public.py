6b06d55b7f894ce2e76f497b5d3c94d2e3477cd7
jyj0line <jyj0line@gmail.com>
2024-11-06 22:19:56 +0900

    Update save_mat_public.py

--- save_mat_init/save_mat_public.py
+++ save_mat_init/save_mat_public.py
@@ -7,7 +7,8 @@ from source.save_meta import save_meta
 meta = {
     "segmentation": "x",
     "value": "raw",
-    "dimension": ("time", "channel")
+    "dimension": ("time", "channel"),
+    "channel_orders": ["Fp1", "Fp2", "F3", "F4", "C3", "C4", "P3", "P4", "O1", "O2", "F7", "F8", "T7", "T8", "P7", "P8", "Fz", "Cz", "Pz"]
 }
 
 def save_mat_public(dir_path_load: str, dir_path_save: str, meta_path_save: str): 
